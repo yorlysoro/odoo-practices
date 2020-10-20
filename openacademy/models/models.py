ca3a72071ebe4dc54e792dfc287971141627c68b
yorlysoro <yorlysoro@gmail.com>
2020-10-20 17:18:22 -0400

    First model Open Academy
    Signed-off-by: yorlysoro <yorlysoro@gmail.com>

---
+++ openacademy/models/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+    _description = "OpenAcademy Courses"
+
+    name = fields.Char(string="Title", required=True)
+    _description = fields.Text()
