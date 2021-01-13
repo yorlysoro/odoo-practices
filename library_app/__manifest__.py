e08dd58e0c6da3b9ad88b1c45f5d7d2c8227a42b
yorlysoro <yorlysoro@gmail.com>
2021-01-12 22:11:38 -0400

    Chapter 02 - Chapter 03

---
+++ library_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Library Management',
+    'description': 'Manage library book catalogue and lending.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+
+    'data': [
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/library_menu.xml',
+        'views/book_view.xml',
+        'views/book_list_template.xml',
+    ],
+}
